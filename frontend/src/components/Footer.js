95f807852c2d2713fd51db4c71cc5b01d391fbd0
ejercicio-01 <nicole.dorola@gmail.com>
2021-08-09 03:30:05 -0300

    cambios

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -14,13 +14,13 @@ const Footer = () => {
                             <NavLink to = "/LOGOUT"><p>LOGOUT</p></NavLink>
                         </nav>
                     </div>
+                    <div className="allRightReserves">miTinerary Proyect 2021 - All Rights Reserved</div>
                     <div className="socialMedia">
                         <Link to="/NotFound"><img src='/assets/instagram.png' alt="instagram"></img></Link>
                         <Link to="/NotFound"><img src='/assets/facebook.png' alt="facebook"></img></Link>
                         <Link to="/NotFound"><img src='/assets/mail.png' alt="mail"></img></Link>
                     </div>
                 </div>
-                <div className="allRightReserves">miTinerary Proyect 2021 - All Rights Reserved</div>
             </div>
         </>
     )
