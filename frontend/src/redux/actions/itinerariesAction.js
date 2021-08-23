da806daac322cef3c166e17db5eb5b2fae5b4f69
ejercicio-01 <nicole.dorola@gmail.com>
2021-08-23 08:28:02 -0300

    actualizacion

--- frontend/src/redux/actions/itinerariesAction.js
+++ frontend/src/redux/actions/itinerariesAction.js
@@ -6,10 +6,10 @@ const itinerariesActions = {
                 let response = await axios.get('http://localhost:4000/api/itineraries/')
                 let data = response.data.response
                 dispatch({type: 'GET_ALL_ITINERARIES', payload: data})
-                if (!response.data.seccess) {
-                    throw new Error ('Ups! The server is down'),
-                    console.log('Ups! The server is down')
-                }
+                // if (!response.data.seccess) {
+                //     throw new Error ('Ups! The server is down'),
+                //     console.log('Ups! The server is down')
+                // }
         }
     }
 }
